b0d2940889fb714106aae9fce004a22bc34b9e5b
Homayoun <homayoun763@gmail.com>
2026-02-12 15:36:47 +0330

    refactor : Add Snippet Button

---
+++ src/components/AddSnippetBtn.tsx
@@ -0,0 +1,20 @@
+import {Plus} from "lucide-react";
+import Button from "./Button.tsx";
+
+const AddSnippetBtn = ({handleClick}: { handleClick: React.MouseEventHandler<HTMLButtonElement> }) => {
+    return (
+        <Button
+            onClick={handleClick}
+            className="bg-white font-comic hover:bg-red-50 text-gray-900 px-4 py-2  rounded-2xl flex items-center gap-2 transition-all hover:scale-105 hover:rotate-1 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] border-3 border-black relative group"
+        >
+            <Plus className="w-4 h-4 group-hover:rotate-90 transition-transform"/>
+            Add Snippet
+            <svg className="absolute -top-2 -right-2 w-6 h-6" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="12" r="8" fill="#fbbf24" stroke="#000" strokeWidth="2"/>
+                <path d="M 8 12 L 12 8 L 16 12" stroke="#000" strokeWidth="2" fill="none"/>
+            </svg>
+        </Button>
+    );
+};
+
+export default AddSnippetBtn;
--- src/components/NoSnippet.tsx
+++ src/components/NoSnippet.tsx
@@ -1,6 +1,6 @@
-import {Copy, Plus} from "lucide-react";
-import Button from "./Button.tsx";
+import {Copy} from "lucide-react";
 import type {ActionType} from "../types.tsx";
+import AddSnippetBtn from "./AddSnippetBtn.tsx";
 
 function NoSnippet({dispatch} : { dispatch : (value: ActionType) => void}) {
     return (
@@ -11,18 +11,7 @@ function NoSnippet({dispatch} : { dispatch : (value: ActionType) => void}) {
             <h2 className="text-xl md:text-2xl font-sketch font-semibold text-gray-600 mb-2">
                 No snippets Found
             </h2>
-            <Button
-                onClick={() => dispatch({ type: "OPEN_FORM" })}
-                className="bg-white font-comic-bold hover:bg-red-50 text-gray-900 px-4 py-2  rounded-2xl flex items-center gap-2 transition-all hover:scale-105 hover:rotate-1 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] border-3 border-black relative group"
-            >
-                <Plus className="w-4 h-4 group-hover:rotate-90 transition-transform" />
-                Add Snippet
-                {/* Doodle decoration */}
-                <svg className="absolute -top-2 -right-2 w-6 h-6" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="12" cy="12" r="8" fill="#fbbf24" stroke="#000" strokeWidth="2"/>
-                    <path d="M 8 12 L 12 8 L 16 12" stroke="#000" strokeWidth="2" fill="none"/>
-                </svg>
-            </Button>
+            <AddSnippetBtn handleClick={() => dispatch({ type: "OPEN_FORM" })} />
         </div>
     );
 }
